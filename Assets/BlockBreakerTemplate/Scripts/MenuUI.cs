5a45f010ec56c606cd788c5527847c66c36cb95b
Shahar <shahar.bar@communix.com>
2023-10-05 16:47:35 +0300

    Full Project added

---
+++ Assets/BlockBreakerTemplate/Scripts/MenuUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour 
+{
+	//Called when the play button is pressed
+	public void PlayButton ()
+	{
+		Application.LoadLevel(1);	//Loads the 'Game' scene to begin the game
+	}
+
+	//Called when the quit button is pressed
+	public void QuitButton ()
+	{
+		Application.Quit();			//Quits the game
+	}
+}
